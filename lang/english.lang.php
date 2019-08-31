f1cd4fdad4288c3a12d05a86b7ee396ca5450084
Cahya DSN <cahyadsn@gmail.com>
2019-09-01 01:28:04 +0700

    lang ver 3.1.4

---
+++ lang/english.lang.php
@@ -0,0 +1,201 @@
+<?php
+/*
+BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
+================================================================================
+filename	: 
+purpose	: 
+create	: 2006/04/24
+last edit	: 060301,17,22-24,28,0412,1124,070430,1217,080810,120118
+author	: cahya dsn
+================================================================================
+This program is free software; you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+copyright (c) 2006-2012 by cahya dsn; cahyadsn@yahoo.com
+================================================================================
+*/
+define('_INSTALL_WARN','For your security please completely remove the <b style="color:red">installation</b> directory including all files and sub-folders  - then refresh this page');
+define('_OFFLINE_MESSAGE','This site is down for maintenance.<br /> Please check back again soon.');
+define('_ERROR_MESSAGE','This site is temporarily unavailable.<br /> Please notify the System Administrator');
+define('_WELCOME','<center>Welcome to Adzan Administration Panel</center>');
+define("_ADD","Add");
+define("_EDIT","Edit");
+define("_FIRST","first");
+define("_PREV","prev");
+define("_NEXT","next");
+define("_LAST","last");
+define("_TODAY","today");
+define("_YESTERDAY","yesterday");
+define("_TOMORROW","tomorrow");
+define("_WIDTH","550");
+define("_CITYNAME","City Name");
+define("_COUNTRY","Country");
+define("_COORDINATE","Coordinate");
+define("_TIMEZONE","Time Zone");
+define("_ACTION","Action");
+define("_LATITUDE","Latitude");
+define("_LONGITUDE","Longitude");
+define("_DST","Daylight Saving Time");
+define("_DATE","Date");
+define("_YEAR","Year");
+define("_MONTH","Month");
+define("_DEG","deg");
+define("_MIN","min");
+define("_HOUR","hour");
+define("_E","E");
+define("_W","W");
+define("_S","S");
+define("_N","N");
+define("_DELETE_MSG","Do you really want to deleted this record ?\\n(There is simulation only, not really deleted)");
+define("_SAVE_MSG","Do you really want to saved this record ?\\n(There is simulation only, not really saved)");
+define("_CANCEL_MSG","Do you really want to cancelled this action ?");
+define("_SAVE","Save");
+define("_CANCEL","Cancel");
+define("_SEARCH","Search");
+define("_SEARCHCITY","Search City");
+
+define("_USERNAME","Username");
+define("_EDITNAME","Edit Username");
+define("_CURRENTNAME","Current username");
+define("_NEWNAME","New username");
+define("_PASSWORD","Password");
+define("_EDITPASSWORD","Edit Password");
+define("_NEWPASSWORD","New password");
+define("_OLDPASSWORD","Old password");
+define("_CONFIRMPASSWORD","Confirm password");
+define("_SELECTCITY","Select City");
+define("_FORCITY","For City");
+define("_SURROUNDING","and its surroundings");
+define("_DIRECTION","Direction");
+define("_DISTANCE","Distance");
+define("_TO","to");
+define("_MECCA","Mecca");
+
+define("_REGISTERNOW","Register Adzan Now!");
+define("_REGISTERCAN","With registered Adzan you can");
+define("_REGISTERCITY","show prayer time schedule for others city whitout change configuration file");
+define("_REGISTERMONTHLY","show monthly and annualy prayer time schedule");
+define("_REGISTERSUPPORT","support Adzan");
+define("_REGISTERINFO","Registration Information");
+define("_REGISTERTO","Registered To");
+define("_REGISTERSTATUS","Registration Status");
+define("_REGISTERONLINE","Online Registration");
+define("_REGISTERENTER","Enter Registration Key");
+define("_REGISTERENTERKEY","Registration Key Entry");
+define("_REGISTERNAME","Registration Name");
+define("_REGISTERKEY","Registration Key");
+define("_REGISTERKEYINVALID","Invalid Key");
+define("_REGISTERNO","Not Registered");
+define("_REGISTERED","Registered");
+
+define("_LAYOUT","Layout");
+define("_ADZANLAYOUT","Adzan Layout");
+define("_ADZANLAYOUTNOTES","Set default view for Adzan period, city, country ,language, style user interface and algorithm to calculate adzan.");
+define("_ADZANVIEWNOTES","Set to show or hide Parameters, Qibla visualization, and Fiqh options. You can also set Time format on hh:mm:dd (default time format is hh:mm)");
+define("_PERIOD","Period");
+define("_DEFAULTCOUNTRY","Default Country");
+define("_DEFAULTCITY","Default City");
+define("_LANGUAGE","Language");
+define("_STYLE","Style");
+define("_ALGORITHM","Algorithm");
+define("_OBSERVERHEIGHT","Observer's Height");
+define("_METERS","meters");
+
+define("_SHOWPARAM","Show Parameters");
+define("_SHOWQIBLA","Show Qibla Visualization");
+define("_SHOWFIQH","Show Fiqh Options");
+define("_SHOWSECOND","Show Time Format with Second");
+define("_SHOWIMSYAK","Show Imsyak Time");
+define("_SHOWSUNRISE","Show Sunrise Time");
+define("_FIQHNOTES","Set fiqh parameters with your suitable values. These values are depend on your opinion for Fajr, Ashr and Isha time.");
+
+define("_FIQH","Fiqh");
+define("_FIQHOPTION","Fiqh Options");
+define("_FIQHMETHOD","Fiqh Method");
+define("_DETERMINATIONOFFAJR","Determination of Fajr");
+define("_SUNDEPRESSION","Sun's depression");
+define("_INTERVALFROMSUNRISE","Interval from Sunrise");
+define("_DETERMINATIONOFASR","Determination of Asr");
+define("_SHAFIIANDOTHERS","Shafi'i and others");
+define("_SHADOWRATIO","Shadow ratio ");
+define("_DETERMINATIONOFISHA","Determination of Isha");
+define("_INTERVALFROMSUNSET","Interval from Sunset");
+define("_DETERMINATIONOFIMSYAK","Determination of Imsyak");
+define("_IMSYAKDEPRESSION","Sun's depression to Fajr");
+define("_INTERVALFROMFAJR","Interval from Fajr");
+
+define("_DETERMINATIONOFIHTIYAT","Determination of Ihtiyat Time");
+define("_IHTIYATTIME","Ihtiyat Time");
+define("_IHTIYATFAJR","Fajr Ihtiyat");
+define("_IHTIYATDZUHR","Dzuhr Ihtiyat");
+define("_IHTIYATASHR","Ashr Ihtiyat");
+define("_IHTIYATMAGHRIB","Maghrib Ihtiyat");
+define("_IHTIYATISHA","Isha Ihtiyat");
+define("_MINUTES","minute(s)");
+
+define("_EXPORTINFOPERIOD","Please select and set appropriate data period to export");
+define("_PERIODOPTIONS","Period Options");
+define("_EXPORTDATAFORMAT","Data Format");
+define("_EXPORTINFOFORMAT","Please select and set appropriate data format to export");
+define("_EXPORTXML","XML Options");
+define("_EXPORTXMLINFO","This format has no options");
+define("_EXPORTEXCEL","Excel Options");
+define("_EXPORTEXCELED","Excel Edition");
+define("_EXPORTCSVEXCEL","CSV for MS Excel");
+define("_EXPORTCSV","CSV Options");
+define("_EXPORTTXT","TXT Options");
+define("_EXPORTFIRST","Put fields names at first row");
+define("_EXPORTFIELDTERMINATED","Fields terminated by");
+define("_EXPORTFIELDENClOSED","Fields enclosed by");
+define("_EXPORTLINETERMINATED","Lines terminated by");
+define("_EXPORTCOMPRESS","Compression");
+define("_EXPORTNONE","None");
+define("_EXPORTGZIP","\"gzipped\"");
+define("_EXPORTZIP","\"zipped\"");
+define("_EXPORTTGZ","tgz");
+define("_EXPORTBZ2","bzip2");
+define("_EXPORTPARAMETER","Parameters");
+define("_EXPORTPARAMETERNOTES","Set parameters for city, country ,language, and algorithm to calculate adzan.");
+define("_EXPORTPARAMETERVIEW","Set to show or hide Parameters, Imsyak time, and Sunrise time. You can also set Time format on hh:mm:dd (default time format is hh:mm)");
+
+define("_USERMANAGEMENT","User Management");
+define("_CITYMANAGEMENT","City Management");
+define("_OPTIONS","Options");
+define("_EXPORT","Export");
+define("_LICENSE","License");
+define("_REGISTRATION","Registration");
+define("_DOCUMENTATION","Documentation");
+define("_ABOUT","About");
+define("_LOGOUT","Logout");
+
+$adzanMsg=array('',
+          'Please login to access this menu',
+          'Access denied ! unauthorized username or/and password',
+          'You enter wrong old password, action cancelled',
+          'Username and Password was updated successfully',
+          'Password was updated successfully',
+          'Username was updated successfully',
+          'Error : unable to update user data',
+          'Preferences data updated successfully',
+          'Error : unable to update preferences data',
+          'Thank you, for registration this program',
+          'Invalid Registration key',
+          'Error: unable to updated configuration file',
+          'Data update successfully');
+$adzanMonth=array("January","February","March","April","May","June","July","August","September","October","November","December");
+$adzanTime=array("Imsyak","Fajr","Syuruq","Dzuhr","Asr","Maghrib","Isha");
+$adzanPeriod=array("Daily","Monthly","Annualy");
+$adzanMethod=array("Custom",
+          "Egyptian General Authority of Survey",
+          "University of Islamic Sciences, Karachi (Shaf'i)",
+          "University of Islamic Sciences, Karachi (Hanafi)",
+          "Islamic Society of North America",
+          "Muslim World League (MWL)",
+          "Umm Al-Qurra, Saudi Arabia",
+          "Fixed Ishaa Interval (always 90)");
+?>
---
+++ lang/indonesian.lang.php
@@ -0,0 +1,201 @@
+<?php
+/*
+BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
+================================================================================
+filename	: 
+purpose	: 
+create	: 2006/04/24
+last edit	: 060301,17,22-24,28,0412,1124,070430,1217,120118
+author	: cahya dsn
+================================================================================
+This program is free software; you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+copyright (c) 2006-2012 by cahya dsn; cahyadsn@yahoo.com
+================================================================================
+*/
+define('_INSTALL_WARN','Untuk keamanan situs Anda, silakan hapus direktori <b style="color:red">installation</b> dan semua file di dalamnya  - kemudian refresh halaman ini');
+define('_OFFLINE_MESSAGE','Situs ini tidak aktif karena sedang dalam perawatan.<br /> Silakan kunjungi kembali beberapa saat lagi.');
+define('_ERROR_MESSAGE','Situs ini untuk sementara tidak aktif<br /> Silakan memberitahu System Administrator');
+define('_WELCOME','<center>Selamat Datang di Adzan Administration Panel</center>');
+define("_ADD","Tambah");
+define("_EDIT","Edit");
+define("_FIRST","awal");
+define("_PREV","sebelum");
+define("_NEXT","sesudah");
+define("_LAST","akhir");
+define("_TODAY","hari ini");
+define("_YESTERDAY","kemarin");
+define("_TOMORROW","besok");
+define("_WIDTH","550");
+define("_CITYNAME","Nama Kota");
+define("_COUNTRY","Negara");
+define("_COORDINATE","Koordinat");
+define("_TIMEZONE","Zona Waktu");
+define("_ACTION","Perintah");
+define("_LATITUDE","Lintang");
+define("_LONGITUDE","Bujur");
+define("_DST","Daylight Saving Time");
+define("_DATE","Tanggal");
+define("_YEAR","Tahun");
+define("_MONTH","Bulan");
+define("_DEG","deg");
+define("_MIN","min");
+define("_HOUR","jam");
+define("_E","BT");
+define("_W","BB");
+define("_S","LS");
+define("_N","LU");
+define("_DELETE_MSG","Anda yakin akan menghapus data tersebut ?\\n(Ini hanya simulasi, tidak benar-benar dihapus)");
+define("_SAVE_MSG","Anda yakin akan menyimpan data tersebut ?\\n(Ini hanya simulasi, tidak benar-benar disimpan)");
+define("_CANCEL_MSG","Anda yakin akan membatalkan perintah ini ?");
+define("_SAVE","Simpan");
+define("_CANCEL","Batal");
+define("_SEARCH","Cari");
+define("_SEARCHCITY","Cari kota");
+
+define("_USERNAME","Nama Pengguna");
+define("_EDITNAME","Ubah Nama Pengguna");
+define("_CURRENTNAME","Nama Pengguna saat ini");
+define("_NEWNAME","Nama Pengguna baru");
+define("_PASSWORD","Kata Sandi");
+define("_EDITPASSWORD","Ubah Kata Sandi");
+define("_NEWPASSWORD","Kata sandi baru");
+define("_OLDPASSWORD","Kata sandi lama");
+define("_CONFIRMPASSWORD","Konfirmasi kata sandi baru");
+define("_SELECTCITY","Pilih Kota");
+define("_FORCITY","Untuk Kota");
+define("_SURROUNDING","dan sekitarnya");
+define("_DIRECTION","Arah");
+define("_DISTANCE","Jarak");
+define("_TO","ke");
+define("_MECCA","Mekah");
+
+define("_REGISTERNOW","Daftarkan Adzan sekarang!");
+define("_REGISTERCAN","Dengan mendaftarkan Adzan, Anda dapat");
+define("_REGISTERCITY","menampilkan jadwal shalat untuk kota-kota lainnya tanpa mengubah file konfigurasi");
+define("_REGISTERMONTHLY","menampilkan jadwal shalat bulanan dan tahunan");
+define("_REGISTERSUPPORT","dukungan terhadap program Adzan");
+define("_REGISTERINFO","Informasi Pendaftaran");
+define("_REGISTERTO","Terdaftar atas nama");
+define("_REGISTERSTATUS","Status pendaftaran");
+define("_REGISTERONLINE","Pendaftaran Online");
+define("_REGISTERENTER","Masukkan Nomor Pendaftaran");
+define("_REGISTERENTERKEY","Pendaftaran");
+define("_REGISTERNAME","Nama");
+define("_REGISTERKEY","Nomor Pendaftaran");
+define("_REGISTERKEYINVALID","Nomor Pendaftaran tidak sesuai");
+define("_REGISTERNO","Tidak terdaftar");
+define("_REGISTERED","Terdaftar");
+
+define("_LAYOUT","Tampilan");
+define("_ADZANLAYOUT","Tampilan Adzan");
+define("_ADZANLAYOUTNOTES","Atur tampilan default untuk periode, negara, kota, bahasa, ragam tampilan dan algoritma yang digunakan dalam perhitungan Adzan.");
+define("_ADZANVIEWNOTES","Atur untuk menampilkan atau tidak Parameter, Visualisasi arah kiblat, dan Pilihan Fiqh. Anda juga dapat mengatur tampilan waktu dalam format hh:mm:dd (secara default format waktu adalah hh:mm)");
+define("_PERIOD","Periode");
+define("_DEFAULTCOUNTRY","Default Negara");
+define("_DEFAULTCITY","Default Kota");
+define("_LANGUAGE","Bahasa");
+define("_STYLE","Ragam Tampilan");
+define("_ALGORITHM","Algoritma");
+define("_OBSERVERHEIGHT","Tinggi daerah pengamatan");
+define("_METERS","meter");
+
+define("_SHOWPARAM","Tampilkan Parameter");
+define("_SHOWQIBLA","Tampilkan Visualisasi Arah Kiblat");
+define("_SHOWFIQH","Tampilkan Pilihan Fiqh");
+define("_SHOWIMSYAK","Tampilkan Waktu Imsyak");
+define("_SHOWSUNRISE","Tampilkan Waktu Matahari Terbit");
+define("_SHOWSECOND","Tampilkan Waktu sampai dengan detik");
+define("_FIQHNOTES","Atur parameter fiqh dengan nilai-nilai yang sesuai dengan Anda. Tergantung pandangan, pemahaman dan interpretasi Anda terhadap waktu Shubuh, Ashr dan Isya.");
+
+define("_FIQH","Fiqh");
+define("_FIQHOPTION","Pilihan Fiqh");
+define("_FIQHMETHOD","Metode Fiqh");
+define("_DETERMINATIONOFFAJR","Penentapan Waktu Shubuh");
+define("_SUNDEPRESSION","Kemiringan Matahari");
+define("_INTERVALFROMSUNRISE","Jarak waktu dari matahari terbit");
+define("_DETERMINATIONOFASR","Penetapan Waktu Ashr");
+define("_SHAFIIANDOTHERS","Shafi'i dan lainnya");
+define("_SHADOWRATIO","Perbandingan bayangan ");
+define("_DETERMINATIONOFISHA","Penetapan Waktu Isya");
+define("_INTERVALFROMSUNSET","Jarak dari matahari terbenam");
+define("_DETERMINATIONOFIMSYAK","Penetapan Waktu Imsyak");
+define("_IMSYAKDEPRESSION","Kemiringan Matahari dari Waktu Shubuh");
+define("_INTERVALFROMFAJR","Jarak Waktu dari Shubuh");
+
+define("_DETERMINATIONOFIHTIYAT","Penetapan Waktu Ihtiyat");
+define("_IHTIYATTIME","Waktu Ihtiyat");
+define("_IHTIYATFAJR","Ihtiyat Subuh");
+define("_IHTIYATDZUHR","Ihtiyat Dzuhur");
+define("_IHTIYATASHR","Ihtiyat Ashr");
+define("_IHTIYATMAGHRIB","Ihtiyat Maghrib");
+define("_IHTIYATISHA","Ihtiyat Isya");
+define("_MINUTES","menit");
+
+define("_EXPORTINFOPERIOD","Silakan pilih dan atur periode data yang akan diekspor");
+define("_PERIODOPTIONS","Pilihan Periode");
+define("_EXPORTDATAFORMAT","Format Data");
+define("_EXPORTINFOFORMAT","Silakan pilih dan atur format data yang akan diekspor");
+define("_EXPORTXML","Pilihan XML");
+define("_EXPORTXMLINFO","Tidak ada pilihan untuk format ini");
+define("_EXPORTEXCEL","Pilihan Excel");
+define("_EXPORTEXCELED","Edisi Excel");
+define("_EXPORTCSVEXCEL","CSV untuk MS Excel");
+define("_EXPORTCSV","Pilihan CSV");
+define("_EXPORTTXT","Pilihan TXT");
+define("_EXPORTFIRST","Tambahkan nama field pada baris pertama");
+define("_EXPORTFIELDTERMINATED","Antar field dibatasi oleh");
+define("_EXPORTFIELDENClOSED","Tiap field diapit oleh");
+define("_EXPORTLINETERMINATED","Antar baris dibatasi oleh");
+define("_EXPORTCOMPRESS","Kompresi");
+define("_EXPORTNONE","Tidak");
+define("_EXPORTGZIP","gzip");
+define("_EXPORTZIP","zip");
+define("_EXPORTTGZ","tgz");
+define("_EXPORTBZ2","bzip2");
+define("_EXPORTPARAMETER","Parameter");
+define("_EXPORTPARAMETERNOTES","Atur parameter untuk negara, kota, bahasa, dan algoritma yang digunakan dalam perhitungan Adzan");
+define("_EXPORTPARAMETERVIEW","Atur untuk menampilkan atau tidak parameter, waktu imsyak dan waktu matahari terbit. Anda juga dapat mengatur tampilan waktu dalam format hh:mm:dd (secara default format waktu adalah hh:mm)");
+
+define("_USERMANAGEMENT","Data Pengguna");
+define("_CITYMANAGEMENT","Data Kota");
+define("_OPTIONS","Pilihan");
+define("_EXPORT","Ekspor");
+define("_LICENSE","Lisensi");
+define("_REGISTRATION","Pendaftaran");
+define("_DOCUMENTATION","Dokumentasi");
+define("_ABOUT","Tentang Program");
+define("_LOGOUT","Keluar");
+
+$adzanMsg=array('',
+          'Silakan login terlebih dahulu sebelum menggunakan menu tersebut',
+          'Nama pengguna dan/atau kata sandi yang dimasukkan tidak sesuai',
+          'Kata sandi lama yang dimasukan salah, perintah dibatalkan',
+          'Nama pengguna dan Kata sandi berhasil diperbaharui',
+          'Kata sandi berhasil diperbaharui',
+          'Username berhasil diperbaharui',
+          'Kesalahan : data pengguna gagal diperbaharui',
+          'Data preferensi berhasil diperbaharui',
+          'Kesalahan : data preferensi gagal diperbaharui',
+          'Terima kasih, Anda telah mendaftarkan program ini',
+          'Registration Key yang Anda masukkan tidak sesuai',
+          'Kesalahan : file konfigurasi gagal diperbaharui',
+          'Data berhasil diperbaharui');
+$adzanMonth=array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+$adzanTime=array("Imsyak","Shubuh","Terbit","Dzuhur","Ashr","Maghrib","Isya");
+$adzanPeriod=array("Harian","Bulanan","Tahunan");
+$adzanMethod=array("Custom",
+          "Egyptian General Authority of Survey",
+          "University of Islamic Sciences, Karachi (Shaf'i)",
+          "University of Islamic Sciences, Karachi (Hanafi)",
+          "Islamic Society of North America",
+          "Muslim World League (MWL)",
+          "Umm Al-Qurra, Saudi Arabia",
+          "Fixed Ishaa Interval (always 90)");
+?>
